baea3288c68541c33a2d98daa446e26f65dd4495
tinyboi2021 <96110963+tinyboi2021@users.noreply.github.com>
2024-10-08 14:51:44 +0530

    Create arithmeticOPR.py

---
+++ python/arithmeticOPR.py
@@ -0,0 +1,10 @@
+int_no1=123
+int_no2=345
+int_neg=-234
+float_no1=0.15
+float_no2=1.25
+
+print(int_no1 + int_neg)
+print(int_no1 + int_no2)
+print(int_no1 + float_no1)
+print(float_no1 + float_no2)
