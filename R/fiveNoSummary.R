8bc79c5e9b08782789fd3970ed0f0f033472c14d
tinyboi2021 <96110963+tinyboi2021@users.noreply.github.com>
2025-02-13 15:45:35 +0530

    Create fiveNoSummary.R

---
+++ R/fiveNoSummary.R
@@ -0,0 +1,18 @@
+data=c(3,55,63,66,88,89,99,232)
+mean(data)
+
+median(data)
+sd(data) 
+var(data)
+
+mean(data,na.rm = TRUE)
+
+
+range(data)
+
+library(plyr)
+  
+quantile(data)
+quart<-quantile(data)
+
+IQR(data)
